836c19063f487dd850dc4b69bd70882df6ab7a47
Georgy <gbvolkov@gmail.com>
2022-04-22 12:54:20 +0300

    commit

--- isexp.ts
+++ isexp.ts
@@ -31,11 +31,36 @@ interface OrderData {
   Receipt: ReceiptData;
 }
 
+interface OrderSubscription {
+  subscriptionId: mongoDB.ObjectId;
+  planId: mongoDB.ObjectId;
+}
+
+interface OrderUserSubscription {
+  userSubscriptionId: mongoDB.ObjectId;
+  period: number;
+}
+
+
 interface Order {
   _id: mongoDB.ObjectId;
   operation: string;
   updateDate: Date;
   data: OrderData;
+  subscriptions: OrderSubscription[];
+  userSubscriptions: OrderUserSubscription[];
+}
+
+interface Plan {
+  _id: mongoDB.ObjectId;
+  period: number;
+  price: number;
+}
+
+interface Subscription {
+  name: string;
+  description: string;
+  plans: Plan[];
 }
 
 interface csvData {
@@ -49,9 +74,105 @@ interface csvData {
 	itemName: string;
 	itemAmount: number;
 	itemPrice: number;
-	itemQtty: number/*;
+	itemQtty: number;
+  subscriptionName: string | undefined;
+  planPeriod: number | undefined;
+  subscriptionId: mongoDB.ObjectId;
+  planId: mongoDB.ObjectId;
+  /*;
 	itemPaymentMethod: string;
 	itemPaymentObject: string;*/
+}
+
+
+
+
+
+
+
+function getSubsciption (db: mongoDB.Db, id: mongoDB.ObjectId): Promise<Subscription | null> {
+  const subscriptions = db.collection(
+    process.env.SUBSCRIPTIONS_COLLECTION as string
+  );
+
+  const result =  subscriptions
+  .findOne<Subscription>({_id: id,});
+  return result;
+}
+
+async function getOrders(db: mongoDB.Db): Promise<csvData[]> {
+  const csvres: csvData[] = [];
+  const orders = db.collection(process.env.ORDERS_COLLECTION as string);
+
+  const query = {
+    operation: { $in: ["PAYMENT", "RENEW"] },
+    "data.Status": "CONFIRMED"
+  };
+
+  const cursor = orders.find<Order>(query, {
+    projection: {
+      _id: 1,
+      operation: 1,
+      updateDate: 1,
+      data: {
+        Status: 1,
+        Amount: 1,
+        CustomerKey: 1,
+        DATA: { email: 1 },
+        Receipt: { Items: 1 },
+      },
+      subscriptions: {
+        subscriptionId: 1,
+        planId: 1,
+      },
+      userSubscriptions: {
+        userSubscriptionId: 1,
+        period: 1,
+      },
+    },
+  });
+
+  const getSubscriptionPromises: Promise<Subscription | null>[] = [];
+  const docs = await cursor.toArray();
+
+  for (const doc of docs) {
+    doc.data.Receipt.Items.forEach(function (item, idx) {
+      var subscriptionId: mongoDB.ObjectId;
+      if (doc.operation === "RENEW") {
+        subscriptionId = doc.userSubscriptions[idx]?.userSubscriptionId;
+      } else {
+        subscriptionId = doc.subscriptions[idx]?.subscriptionId
+      }
+      csvres.push({
+        orderId: doc._id.toHexString(),
+        email: doc.data.DATA.email,
+        customerKey: doc.data.CustomerKey,
+        operation: doc.operation,
+        status: doc.data.Status,
+        updateDate: doc.updateDate,
+        orderAmount: doc.data.Amount,
+        itemName: item.Name,
+        itemAmount: item.Amount,
+        itemPrice: item.Price,
+        itemQtty: item.Quantity,
+        subscriptionId: subscriptionId,
+        planId: doc.subscriptions[idx]?.planId,
+        subscriptionName: "",
+        planPeriod: doc.userSubscriptions[idx]?.period,
+      });
+      getSubscriptionPromises.push(getSubsciption(db, subscriptionId));
+    });
+  }
+
+  const subscriptions = await Promise.all(getSubscriptionPromises);
+  csvres.forEach(function(row, idx) {
+    row.subscriptionName = subscriptions[idx]?.name;
+    if (row.operation === "PAYMENT") {
+      row.planPeriod = subscriptions[idx]?.plans.find(plan => plan._id.toHexString()===row.planId?.toHexString())?.period;
+    }
+  });
+
+  return csvres;
 }
 
 
@@ -65,69 +186,29 @@ const main = async () => {
   try {
     await client.connect();
 
-    const db: mongoDB.Db = client.db(process.env.DB_NAME);
-    const orders: mongoDB.Collection = db.collection(
-      process.env.ORDERS_COLLECTION as string
-    );
-    const query = {
-      operation: { $in: ["PAYMENT", "RENEW"] },
-      "data.Status": "CONFIRMED",
-    };
-
-    const cursor = orders.find<Order>(query, {
-      projection: {
-        _id: 1,
-        operation: 1,
-        updateDate: 1,
-        data: {
-          Status: 1,
-          Amount: 1,
-          CustomerKey: 1,
-          DATA: { email: 1 },
-          Receipt: { Items: 1 },
-        },
-      },
-    });
-	const csvw = csv_writer.createObjectCsvWriter({
-		path: 'orders.csv',
-		header: [
-			{id: 'orderId', title: 'orderID'},
-			{id: 'email', title: 'email'},
-			{id: 'customerKey', title: 'CustomerKey'},
-			{id: 'operation', title: 'operation'},
-			{id: 'status', title: 'Status'},
-			{id: 'updateDate', title: 'updateDate'},
-			{id: 'orderAmount', title: 'OrderAmount'},
-			{id: 'itemName', title: 'ItemName'},
-			{id: 'itemAmount', title: 'ItemAmount'},
-			{id: 'itemPrice', title: 'ItemPrice'},
-			{id: 'itemQtty', title: 'ItemQuantity'},
-		]
-	});
-
-	const csvres: csvData[] = [];
-
-	await cursor.forEach(function (doc) {
-      doc.data.Receipt.Items.forEach(function (item) {
-		  csvres.push({
-			orderId: doc._id.toHexString(),
-			email: doc.data.DATA.email,
-			customerKey: doc.data.CustomerKey,
-			operation: doc.operation,
-			status: doc.data.Status,
-			updateDate: doc.updateDate,
-			orderAmount: doc.data.Amount,
-			itemName: item.Name,
-			itemAmount: item.Amount,
-			itemPrice: item.Price,
-			itemQtty: item.Quantity
-		  });
-      });
+    const db = client.db(process.env.DB_NAME);
+    const csvw = csv_writer.createObjectCsvWriter({
+      path: "orders.csv",
+      header: [
+        { id: "orderId", title: "orderID" },
+        { id: "email", title: "email" },
+        { id: "customerKey", title: "CustomerKey" },
+        { id: "operation", title: "operation" },
+        { id: "status", title: "Status" },
+        { id: "updateDate", title: "updateDate" },
+        { id: "orderAmount", title: "OrderAmount" },
+        { id: "itemName", title: "ItemName" },
+        { id: "itemAmount", title: "ItemAmount" },
+        { id: "itemPrice", title: "ItemPrice" },
+        { id: "itemQtty", title: "ItemQuantity" },
+        { id: "subscriptionName", title: "subscriptionName" },
+        { id: "planPeriod", title: "planPeriod" },
+      ],
     });
 
-	csvw.writeRecords(csvres)
-	.then(()=> console.log('The CSV file was written successfully'));
-
+    const csvres = await getOrders(db);
+    csvw.writeRecords(csvres)
+    .then(() => console.log("The CSV file was written successfully"));
   } finally {
     await client.close();
   }
